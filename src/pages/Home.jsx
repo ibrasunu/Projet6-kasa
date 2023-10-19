d6b8f8ca961b1e500da013b113ffd7affece5895
Ibrahima <ibrasunu@gmail.com>
2023-10-19 19:32:32 +0200

    Home page articles created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Gallery from "../components/Gallery";
 import imgHomeBanner from "../assets/homeBanner.png"
 
 
@@ -11,6 +12,7 @@ const Home = () => {
       <Header />
       <main>
         <Banner texte="Chez vous, partout et ailleurs" image={imgHomeBanner} />
+        <Gallery />
       </main>
       <Footer />
     </div>
