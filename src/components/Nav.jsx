af721ec01840f2cdf6f66c502f6cda820066c848
Ibrahima <ibrasunu@gmail.com>
2023-10-26 17:36:56 +0200

    acive link underline updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,10 +4,10 @@ import { NavLink} from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="nav-header">
-      <NavLink to="/" className={({isActive}) => isActive ? "nav-header__nav-link-home active" : "nav-header__nav-link-home "} >
+      <NavLink to="/" className={({isActive}) => isActive ? "nav-header__nav-link-home active" : "nav-header__nav-link-home "}>
         Accueil
       </NavLink>
-      <NavLink to="/about" className={({isActive}) => isActive ? "nav-header__nav-link-about active" : "nav-header__nav-link-about "} >
+      <NavLink to="/about" className={({isActive}) => isActive ? "nav-header__nav-link-about active" : "nav-header__nav-link-about "}>
         A Propos
       </NavLink>
     </nav>
